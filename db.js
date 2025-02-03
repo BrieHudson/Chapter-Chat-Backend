d1a720b82a514d2c4b4fa7d5fc3c3a7285c47b17
Brieana Hudson <brieanahusdon2018@yahoo.com>
2025-02-03 04:08:05 -0500

    final config

--- db.js
+++ db.js
@@ -10,6 +10,13 @@ if (!process.env.DATABASE_URL) {
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    },
+    keepAlive: true,
+  },
   pool: {
     max: 5,
     min: 0,
